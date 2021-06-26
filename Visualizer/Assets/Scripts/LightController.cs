e29a0724b59e3f509b15ce3534bb22497b6c6701
Alex Clément Dos Santos <alex.clementdossantos@gmail.com>
2021-06-26 13:19:17 -0400

    Additionnal light and mesh controls

--- Visualizer/Assets/Scripts/LightController.cs
+++ Visualizer/Assets/Scripts/LightController.cs
@@ -6,16 +6,37 @@ public class LightController : MonoBehaviour
     public float maxIntensity = 10f;
     public string lightName = "Light";
 
+    public GameObject rotateTarget;
+
     private Light directionalLight;
+
+    private float yaw;
+    private float pitch;
 
     void Start()
     {
+        Vector3 eulerRot = gameObject.transform.rotation.eulerAngles;
+        
         directionalLight = GetComponent<Light>();
-        UI.RequestSliderControl(OnSliderValueChanged, $"{lightName} intensity", "Lighting", initial: (directionalLight.intensity - minIntensity) / maxIntensity);
+        UI.RequestSliderControl(OnIntensityChanged, $"{lightName} intensity", "Lighting", initial: (directionalLight.intensity - minIntensity) / maxIntensity);
+        UI.RequestSliderControl(OnYawChanged, $"{lightName} yaw", "Lighting", 0, 359, 0);
+        UI.RequestSliderControl(OnPitchChanged, $"{lightName} pitch", "Lighting", 0, 180, 45);
     }
 
-    public void OnSliderValueChanged(float t)
+    public void OnIntensityChanged(float ratio)
     {
-        directionalLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, t);
+        directionalLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, ratio);
+    }
+
+    public void OnYawChanged(float angle)
+    {
+        yaw = angle;
+        (rotateTarget ?? gameObject).transform.rotation = Quaternion.Euler(pitch, yaw, 0);
+    }
+
+    public void OnPitchChanged(float angle)
+    {
+        pitch = angle;
+        (rotateTarget ?? gameObject).transform.rotation = Quaternion.Euler(pitch, yaw, 0);
     }
 }
--- Visualizer/Assets/Scripts/MeshController.cs
+++ Visualizer/Assets/Scripts/MeshController.cs
@@ -6,15 +6,45 @@ public class MeshController : MonoBehaviour
 {
     public float minScale;
     public float maxScale;
-    
+
+    private float yaw;
+    private float pitch;
+    private float roll;
+
     void Start()
     {
-        UI.RequestSliderControl(OnSliderValueChanged, "Mesh scale", "Mesh", initial: (1 - minScale) / maxScale);
+        UI.RequestSliderControl(OnSliderValueChanged, "Scale", "Mesh", initial: (1 - minScale) / maxScale);
+        UI.RequestSliderControl(OnYawChanged, "Yaw", "Mesh", 0, 359, 0);
+        UI.RequestSliderControl(OnPitchChanged, "Pitch", "Mesh", 0, 359, 0);
+        UI.RequestSliderControl(OnRollChanged, "Roll", "Mesh", 0, 359, 0);
     }
 
-    private void OnSliderValueChanged(float t)
+    private void OnSliderValueChanged(float ratio)
     {
-        float scale = Mathf.Lerp(minScale, maxScale, t);
+        float scale = Mathf.Lerp(minScale, maxScale, ratio);
         gameObject.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    private void OnYawChanged(float angle)
+    {
+        yaw = angle;
+        Rotate();
+    }
+
+    private void OnPitchChanged(float angle)
+    {
+        pitch = angle;
+        Rotate();
+    }
+
+    private void OnRollChanged(float angle)
+    {
+        roll = angle;
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(yaw, pitch, roll);
     }
 }
